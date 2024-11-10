8247d3c27a580fc38da952d7234f03278ae19837
Kartik Aggarwal <kartikaggarwal2004@gmail.com>
2024-11-10 13:24:06 +0530

    last try

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    swcMinify: true, // Enables faster minification
+    experimental: {
+      appDir: true, // Required if you are using the app directory structure
+    },
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
+  
